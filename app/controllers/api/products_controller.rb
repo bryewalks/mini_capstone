0a17ff786e6868977589b3dcfb40a76607fcd311
bryewalks <bryewalks@gmail.com>
2019-01-16 21:42:25 -0600

    all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,31 @@
 class Api::ProductsController < ApplicationController
+  def first_product
+    @product = Product.first
+    render 'first_product.json.jbuilder'    
+  end
+
+  def all_products
+    @products = Product.all
+    render 'all_products.json.jbuilder'
+    
+  end
+  # def video_card2
+  #   @video_card = Products.id(2)
+  #   render 'video_card2.json.jbuilder'    
+  # end
+
+  # def video_card3
+  #   @video_card = Products.id(3)
+  #   render 'video_card3.json.jbuilder'    
+  # end
+
+  # def video_card4
+  #   @video_card = Products.id(4)
+  #   render 'video_card4.json.jbuilder'    
+  # end
+
+  # def video_card5
+  #   @video_card = Products.id(5)
+  #   render 'video_card5.json.jbuilder'    
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+# product = Product.new()
+# product.save
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
