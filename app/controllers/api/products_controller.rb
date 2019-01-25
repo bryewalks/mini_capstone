e8535deaa1ecfe4b4cda5f62b4f8a8487dd92c3a
bryewalks <bryewalks@gmail.com>
2019-01-25 17:37:16 -0600

    friday work

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,31 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    search_keyword = params[:search]
+    sort_keyword = params[:sort]
+    sort_order = params[:sort_order]
+    discount = params[:discount]
+
+    
+    if search_keyword
+      @products = @products.where("name ilike ?", "%#{search_keyword}%")
+    end
+
+    if discount
+      @products = @products.where("price < ?", 10)
+    end
+
+    if sort_keyword && sort_order
+      @products = @products.order(sort_keyword => sort_order)
+    elsif sort_keyword
+      @products = @products.order(sort_keyword)
+    else
+      @products = @products.order(:id)
+    end
+
+
+
     render 'index.json.jbuilder'
   end
 
@@ -9,10 +34,14 @@ class Api::ProductsController < ApplicationController
                             name: params[:name],
                             price: params[:price],
                             image_url: params[:image_url],
-                            description: params[:description]
+                            description: params[:description],
+                            supplier_id: params[:supplier_id]
                             )
-    @product.save
-    render 'show.json.jbuilder'
+    if @product.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -28,9 +57,13 @@ class Api::ProductsController < ApplicationController
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
     @product.in_stock = params[:in_stock] || @product.in_stock
+    @product.supplier_id = params[:supplier_id] || @product.supplier_id
 
-    @product.save
-    render 'show.json.jbuilder'    
+    if @product.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end   
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,22 @@
 class Product < ApplicationRecord
+
+validates :name, presence: true
+validates :name, uniqueness: true
+validates :name, length: {maximum: 254}
+
+validates :price, numericality: {less_than: 100_000_000, greater_than: 0}
+validates :price, presence: true
+
+validates :description, presence: true
+validates :description, length: {in: 10..300}
+
+
+
+  def supplier
+    Supplier.find_by(id: self.supplier_id)
+    
+  end
+ 
   def is_discounted?
     price < 150   
   end
